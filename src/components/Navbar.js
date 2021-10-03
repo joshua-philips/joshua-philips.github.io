294cd38f50a221625825dbe8ed5222d5840650fd
joshuaphilips <philipsjoshua96@gmail.com>
2021-10-03 14:23:56 +0000

    initial/skeleton/setup/navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,138 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav
+        className="
+        navbar navbar-expand-lg
+        bg-black
+        navbar-dark
+        fixed-top
+        py-3
+        bg-opacity-75
+      "
+      >
+        <div className="container">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navmenu"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navmenu">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Bio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Skills
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="/"
+                  className="nav-link btn btn-warning text-dark"
+                  data-bs-toggle="modal"
+                  data-bs-target="#contactModal"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* Modal */}
+      <div
+        className="modal fade"
+        id="contactModal"
+        tabindex="-1"
+        aria-labelledby="contactModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content bg-dark text-light">
+            <div className="modal-header border-bottom-0">
+              <h5 className="modal-title" id="contactModalLabel">
+                <span className="border-bottom border-warning pb-2">
+                  CONTACT
+                </span>
+              </h5>
+              <button
+                type="button"
+                className="btn-close btn-close-white"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form id="form-modal">
+                <div className="mb-3">
+                  <label for="email-modal" className="col-form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email-modal"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="subject-modal" className="form-label">
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="subject-modal"
+                    placeholder=""
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="message-modal" className="col-form-label">
+                    Message
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="message-modal"
+                    required
+                  ></textarea>
+                </div>
+                <div className="modal-footer border-top-0">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="submit" className="btn btn-warning">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
