47fc7984cb46ec19ae5660f5050ffece4d98b582
joshuaphilips <philipsjoshua96@gmail.com>
2023-03-18 12:11:41 +0000

    about update

--- src/data/aboutText.js
+++ src/data/aboutText.js
@@ -1,7 +1,7 @@
 const aboutText = [
   "For as long as I can remember I've always had a passion for solving problems using technology. This passion along with my curiosity of how computers work is what drove me to pursue a career in software development.",
   'Being born to parents whose lives revolved around a diverse range of books influenced my drive to always keep learning. My theologian father was always looking to read anything his hands could grab a hold of no matter how obscure the subject. Constantly seeing that as a child made me curious of discoveries the world has to offer. In a very dynamic field like software development, this is not a bad attribute to have.',
-  "Like any other field, software developers require experience to become better. I love undertaking projects and challenging myself, learning new things in the process. I'm looking for opportunities to join a team where I can apply my skills, learn new things and gain more experience becoming a better developer in the process.",
+  "Like any other field, software developers require experience to become better. I love undertaking projects and challenging myself, learning new things in the process. I'm looking for opportunities to join a team where I can apply my skills, learn new things and gain more experience while becoming a better developer.",
   'In my spare time I read and watch old films. I am very passionate about music; playing the bass and singing in church anytime I can.',
 ];
 
